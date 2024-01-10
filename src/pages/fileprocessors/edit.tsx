b17a934fa3ab5fb453997b1804f3aaf1a7904535
Jakub Wąsala <jakub.wasala1@gmail.com>
2024-01-10 22:45:48 +0100

    added file processor CRUD actions

---
+++ src/pages/fileprocessors/edit.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { IResourceComponentsProps } from "@refinedev/core";
+import { Edit, useForm } from "@refinedev/antd";
+import { Form, Input } from "antd";
+
+export const FileProcessorEdit: React.FC<IResourceComponentsProps> = () => {
+    const { formProps, saveButtonProps } = useForm();
+
+    return (
+        <Edit saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Id"
+                    name={["id"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input readOnly disabled />
+                </Form.Item>
+                <Form.Item
+                    label="Name"
+                    name={["name"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Description"
+                    name={["description"]}
+                    rules={[
+                        {
+                            required: false,
+                        },
+                    ]}
+                >
+                    <Input.TextArea />
+                </Form.Item>
+                <Form.Item
+                    label="Callable"
+                    name={["callable"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Edit>
+    );
+};
---
+++ src/pages/fileprocessors/index.ts
@@ -0,0 +1,4 @@
+export { FileProcessorCreate } from "./create";
+export { FileProcessorEdit } from "./edit";
+export { FileProcessorList } from "./list";
+export { FileProcessorShow } from "./show";
