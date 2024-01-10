559acc34c93d594d9e76307d6c65c0e0aab164c0
Jakub Wąsala <jakub.wasala1@gmail.com>
2024-01-10 20:40:32 +0100

    added edit file page

--- src/pages/files/edit.tsx
+++ src/pages/files/edit.tsx
@@ -1,6 +1,121 @@
+import React from "react";
 import { IResourceComponentsProps } from "@refinedev/core";
-import { AntdEditInferencer } from "@refinedev/inferencer/antd";
+import { Edit, useForm, useSelect } from "@refinedev/antd";
+import { Form, Input, Select } from "antd";
 
 export const FileEdit: React.FC<IResourceComponentsProps> = () => {
-  return <AntdEditInferencer />;
+    const { formProps, saveButtonProps, queryResult } = useForm();
+
+    const filesData = queryResult?.data?.data;
+
+    
+    const { selectProps: formatSelectProps } = useSelect({
+      resource: "fileformats",
+      optionLabel: "format",
+    });
+
+    const { selectProps: processorSelectProps } = useSelect({
+        resource: "fileprocessors",
+        optionLabel: "name",
+    });
+
+    const { selectProps: linkedProcessSelectProps } = useSelect({
+        resource: "processes",
+        optionLabel: "code",
+    });
+
+    return (
+        <Edit saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Id"
+                    name={["id"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input readOnly disabled />
+                </Form.Item>
+<Form.Item
+                    label="Code"
+                    name={["code"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Description"
+                    name={["description"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input.TextArea />
+                </Form.Item>
+                <Form.Item
+                    label="Format"
+                    name={"format"}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Select {...formatSelectProps} />
+                </Form.Item>
+                <Form.Item
+                    label="Processor"
+                    name={"processor"}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Select {...processorSelectProps} />
+                </Form.Item>
+                <Form.Item
+                    label="System params"
+                    name={"system_params"}
+                    rules={[
+                        {
+                            required: false,
+                        },
+                    ]}
+                >
+                    <Input.TextArea disabled />
+                </Form.Item>
+                <Form.Item
+                    label="User params"
+                    name={"user_params"}
+                    rules={[
+                        {
+                            required: false,
+                        },
+                    ]}
+                >
+                    <Input.TextArea />
+                </Form.Item>
+                <Form.Item
+                    label="Linked process"
+                    name={"linked_process"}
+                    rules={[
+                        {
+                            required: false,
+                        },
+                    ]}
+                >
+                    <Select {...linkedProcessSelectProps} />
+                </Form.Item>
+            </Form>
+        </Edit>
+    );
 };
