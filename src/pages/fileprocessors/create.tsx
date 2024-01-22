85f08ee146f35f6b160b9fbb511fe4601c9dae6c
Jakub Wąsala <jakub.wasala1@gmail.com>
2024-01-22 08:49:06 +0100

    added handling of server-side validation errors

--- src/pages/fileprocessors/create.tsx
+++ src/pages/fileprocessors/create.tsx
@@ -2,47 +2,49 @@ import { Create, useForm } from "@refinedev/antd";
 import { IResourceComponentsProps } from "@refinedev/core";
 import { Form, Input } from "antd";
 import React from "react";
+import { ErrorNotifications } from "../../components/error-notifications";
 
 export const FileProcessorCreate: React.FC<IResourceComponentsProps> = () => {
-    const { formProps, saveButtonProps } = useForm();
+  const { formProps, saveButtonProps } = useForm();
 
-    return (
-        <Create saveButtonProps={saveButtonProps}>
-            <Form {...formProps} layout="vertical">
-                <Form.Item
-                    label="Name"
-                    name={["name"]}
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    label="Description"
-                    name={["description"]}
-                    rules={[
-                        {
-                            required: false,
-                        },
-                    ]}
-                >
-                    <Input.TextArea />
-                </Form.Item>
-                <Form.Item
-                    label="Callable"
-                    name={["callable"]}
-                    rules={[
-                        {
-                            required: true,
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-            </Form>
-        </Create>
-    );
+  return (
+    <Create saveButtonProps={saveButtonProps}>
+      <Form {...formProps} layout="vertical">
+        <ErrorNotifications formProps={formProps} />
+        <Form.Item
+          label="Name"
+          name={["name"]}
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Description"
+          name={["description"]}
+          rules={[
+            {
+              required: false,
+            },
+          ]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item
+          label="Callable"
+          name={["callable"]}
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </Create>
+  );
 };
