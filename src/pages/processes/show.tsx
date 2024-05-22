5d54a0c72d7afe786b9b0f0036cfbc278bf68507
Zakariah Nuccio <zak.nuccio@crugroup.com>
2024-05-22 16:40:06 +0100

    Revert "Sort tags in files and processes"
    This reverts commit bfdaf60479b3983b387f46ea778dcb2fc0fa51c8.

--- src/pages/processes/show.tsx
+++ src/pages/processes/show.tsx
@@ -68,9 +68,7 @@ export const ProcessShow: React.FC<IResourceComponentsProps> = () => {
       <Title level={5}>Description</Title>
       <TextField value={record?.description} />
       <Title level={5}>Tags</Title>
-      <Typography.Paragraph>
-        {record?.tags?.toSorted((a: string, b: string) => a.localeCompare(b)).map((tag: string) => <Tag key={tag}>{tag}</Tag>)}
-      </Typography.Paragraph>
+      <Typography.Paragraph>{record?.tags?.map((tag: string) => <Tag key={tag}>{tag}</Tag>)}</Typography.Paragraph>
       <Title level={5}>Executor</Title>
       <Typography.Paragraph>
         {record?.executor &&
